4ccefd4d1d1dbcb450e8515ca048e3fae6259169
GenaPlem <shainkill1337@gmail.com>
2024-02-19 12:34:59 +0000

    Update SoundButton with using drumSounds array

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,19 +1,12 @@
 import {SoundButton} from "./SoundButton";
+import {drumSounds} from "../data/drumSounds";
 
 export const Drumpad = () => {
     return (
         <div className="drumpad">
-            <SoundButton key={'Q'} type={'keys'}/>
-            <SoundButton key={'W'} type={'keys'}/>
-            <SoundButton key={'E'} type={'keys'}/>
 
-            <SoundButton key={'A'} type={'pad'}/>
-            <SoundButton key={'S'} type={'pad'}/>
-            <SoundButton key={'D'} type={'pad'}/>
-
-            <SoundButton key={'Z'} type={'drums'}/>
-            <SoundButton key={'X'} type={'drums'}/>
-            <SoundButton key={'C'} type={'drums'}/>
+            {drumSounds.map(drumSound => <SoundButton btn={drumSound.btn} type={drumSound.type}
+                                                      sound={drumSound.audio}/>)}
 
         </div>
     )
