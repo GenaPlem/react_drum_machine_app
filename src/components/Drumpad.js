26548d3977e35f31ecbdc580223ff86132f30356
GenaPlem <shainkill1337@gmail.com>
2024-02-19 12:12:03 +0000

    Add SoundButtons to Drumpad component

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,6 +1,19 @@
+import {SoundButton} from "./SoundButton";
+
 export const Drumpad = () => {
     return (
         <div className="drumpad">
+            <SoundButton key={'Q'} type={'keys'}/>
+            <SoundButton key={'W'} type={'keys'}/>
+            <SoundButton key={'E'} type={'keys'}/>
+
+            <SoundButton key={'A'} type={'pad'}/>
+            <SoundButton key={'S'} type={'pad'}/>
+            <SoundButton key={'D'} type={'pad'}/>
+
+            <SoundButton key={'Z'} type={'drums'}/>
+            <SoundButton key={'X'} type={'drums'}/>
+            <SoundButton key={'C'} type={'drums'}/>
 
         </div>
     )
