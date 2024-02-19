2a09e1247a13e13dec08c1e12d4a2435b2ab9442
GenaPlem <shainkill1337@gmail.com>
2024-02-19 14:21:30 +0000

    Add function to play sound by click on each element

--- src/components/SoundButton.js
+++ src/components/SoundButton.js
@@ -1,8 +1,12 @@
 export const SoundButton = ({btn, type, sound}) => {
-    // Key, Type, Sound
+    // Btn(KeyboardKey), Key, Type, Sound
+
+    const playSound = (e) => {
+        e.target.children[1].play()
+    }
 
     return (
-        <div className={`drumpad__button-${type}`}>
+        <div className={`drumpad__button-${type}`} onClick={playSound}>
             <span className={'drumpad__key'}>{btn}</span>
             <audio className="clip" id={btn} src={sound}></audio>
         </div>
