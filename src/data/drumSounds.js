7a679f205643e999b1b853b482e74e315187e446
GenaPlem <shainkill1337@gmail.com>
2024-02-19 13:04:20 +0000

    Add sounds to assets folder

--- src/data/drumSounds.js
+++ src/data/drumSounds.js
@@ -1,47 +1,56 @@
 export const drumSounds = [
     {
+        id: 1,
         btn: 'Q',
         type: 'keys',
-        audio: ''
+        audio: '../assets/audio/Heater-1.mp3'
     },
     {
+        id: 2,
         btn: 'W',
         type: 'keys',
-        audio: ''
+        audio: '../assets/audio/Heater-2.mp3'
     },
     {
+        id: 3,
         btn: 'E',
         type: 'keys',
-        audio: ''
+        audio: '../assets/audio/Heater-3.mp3'
     },
     {
+        id: 4,
         btn: 'A',
         type: 'pad',
-        audio: ''
+        audio: '../assets/audio/Heater-4_1.mp3'
     },
     {
+        id: 5,
         btn: 'S',
         type: 'pad',
-        audio: ''
+        audio: '../assets/audio/Dsc_Oh.mp3'
     },
     {
+        id: 6,
         btn: 'D',
         type: 'pad',
-        audio: ''
+        audio: '../assets/audio/Cev_H2.mp3'
     },
     {
+        id: 7,
         btn: 'Z',
         type: 'drums',
-        audio: ''
+        audio: '../assets/audio/Heater-6.mp3'
     },
     {
+        id: 8,
         btn: 'X',
         type: 'drums',
-        audio: ''
+        audio: '../assets/audio/Kick_n_Hat.mp3'
     },
     {
+        id: 9,
         btn: 'C',
         type: 'drums',
-        audio: ''
+        audio: '../assets/audio/RP4_KICK_1.mp3'
     },
 ]
