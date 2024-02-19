1f9130a999145dbb3f078015540225310b815930
GenaPlem <shainkill1337@gmail.com>
2024-02-19 15:18:31 +0000

    Add function to handle key down event and play sound

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.scss';
 import {Drumpad} from "./components/Drumpad";
+import {useEffect} from "react";
 
 function App() {
+
+    const handleKeyDown = (e) => {
+        if (document.getElementById(e.key.toUpperCase()) !== null) {
+            document.getElementById(e.key.toUpperCase()).play()
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown)
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [])
+
     return (
         <div className="App">
             <Drumpad/>
