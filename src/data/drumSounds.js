4fd73d020c2e5ecab7efe8f449de85ab237ca8d1
GenaPlem <shainkill1337@gmail.com>
2024-02-19 14:22:55 +0000

    Add require for each audio file in drumSounds array

--- src/data/drumSounds.js
+++ src/data/drumSounds.js
@@ -3,54 +3,54 @@ export const drumSounds = [
         id: 1,
         btn: 'Q',
         type: 'keys',
-        audio: '../assets/audio/Heater-1.mp3'
+        audio: require('../assets/audio/Heater-1.mp3')
     },
     {
         id: 2,
         btn: 'W',
         type: 'keys',
-        audio: '../assets/audio/Heater-2.mp3'
+        audio: require('../assets/audio/Heater-2.mp3')
     },
     {
         id: 3,
         btn: 'E',
         type: 'keys',
-        audio: '../assets/audio/Heater-3.mp3'
+        audio: require('../assets/audio/Heater-3.mp3')
     },
     {
         id: 4,
         btn: 'A',
         type: 'pad',
-        audio: '../assets/audio/Heater-4_1.mp3'
+        audio: require('../assets/audio/Heater-4_1.mp3')
     },
     {
         id: 5,
         btn: 'S',
         type: 'pad',
-        audio: '../assets/audio/Dsc_Oh.mp3'
+        audio: require('../assets/audio/Dsc_Oh.mp3')
     },
     {
         id: 6,
         btn: 'D',
         type: 'pad',
-        audio: '../assets/audio/Cev_H2.mp3'
+        audio: require('../assets/audio/Cev_H2.mp3')
     },
     {
         id: 7,
         btn: 'Z',
         type: 'drums',
-        audio: '../assets/audio/Heater-6.mp3'
+        audio: require('../assets/audio/Heater-6.mp3')
     },
     {
         id: 8,
         btn: 'X',
         type: 'drums',
-        audio: '../assets/audio/Kick_n_Hat.mp3'
+        audio: require('../assets/audio/Kick_n_Hat.mp3')
     },
     {
         id: 9,
         btn: 'C',
         type: 'drums',
-        audio: '../assets/audio/RP4_KICK_1.mp3'
+        audio: require('../assets/audio/RP4_KICK_1.mp3')
     },
 ]
