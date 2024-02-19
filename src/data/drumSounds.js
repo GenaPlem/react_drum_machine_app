8eee71db164bbe0324574bd7a1411527492f3df6
GenaPlem <shainkill1337@gmail.com>
2024-02-19 12:34:28 +0000

    Add drumSounds data array

---
+++ src/data/drumSounds.js
@@ -0,0 +1,47 @@
+export const drumSounds = [
+    {
+        btn: 'Q',
+        type: 'keys',
+        audio: ''
+    },
+    {
+        btn: 'W',
+        type: 'keys',
+        audio: ''
+    },
+    {
+        btn: 'E',
+        type: 'keys',
+        audio: ''
+    },
+    {
+        btn: 'A',
+        type: 'pad',
+        audio: ''
+    },
+    {
+        btn: 'S',
+        type: 'pad',
+        audio: ''
+    },
+    {
+        btn: 'D',
+        type: 'pad',
+        audio: ''
+    },
+    {
+        btn: 'Z',
+        type: 'drums',
+        audio: ''
+    },
+    {
+        btn: 'X',
+        type: 'drums',
+        audio: ''
+    },
+    {
+        btn: 'C',
+        type: 'drums',
+        audio: ''
+    },
+]
